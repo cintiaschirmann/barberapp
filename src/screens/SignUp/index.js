c56e897b0515b567fc90ea4c9099eb8e5d59a88f
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-26 17:30:13 -0300

    tela SignUp iniciada

---
+++ src/screens/SignUp/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const SignUp = () => {
+  return (
+    <S.Container>
+      <S.Title>TA FUNCIONANDO</S.Title>
+    </S.Container>
+  );
+};
+
+export default SignUp;
