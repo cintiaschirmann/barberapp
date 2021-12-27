4677c8b9210b7f7f0563bcb2f7d55305bfdcf172
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-26 22:18:47 -0300

    inserindo componente input

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import * as S from './styles';
 
+import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
+import {SignInput} from '../../components';
+
 const SignIn = () => {
   return (
     <S.Container>
-      <S.Title>OI CARALHO</S.Title>
+      <BarberLogo width="100%" height="160" />
+      <S.InputArea>
+        <SignInput IconSvg={EmailIcon} placeholder="Digite seu e-mail" />
+        <SignInput IconSvg={LockIcon} placeholder="Digite sua senha" />
+        <S.CustomButton>
+          <S.CustomButtomText>Login</S.CustomButtomText>
+        </S.CustomButton>
+      </S.InputArea>
+
+      <S.SignMessage>
+        <S.MessageButton>Ainda não tem uma conta?</S.MessageButton>
+        <S.MessageButtonBold>CADASTRE-SE AQUI!</S.MessageButtonBold>
+      </S.SignMessage>
     </S.Container>
   );
 };
