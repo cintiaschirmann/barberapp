8850001b9908385dc3ee43e7f64bebfd7c693a96
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-26 22:17:20 -0300

    criando context

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,7 +1,7 @@
-import React, {userContext, useReducer} from 'react';
+import React, {createContext, useReducer} from 'react';
 import {initialState, UserReducer} from '../reducers/UserReducer';
 
-export const UserContext = userContext();
+export const UserContext = createContext();
 
 export default ({children}) => {
   const [state, dispatch] = useReducer(UserReducer, initialState);
