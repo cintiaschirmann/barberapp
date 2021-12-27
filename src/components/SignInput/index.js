a65c7dab5addc6f57151dc40a73f0c67120fc0c1
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-26 23:03:05 -0300

    novos components add

--- src/components/SignInput/index.js
+++ src/components/SignInput/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import * as S from './styles';
 
-const SignInput = ({IconSvg, placeholder}) => {
+const SignInput = ({IconSvg, placeholder, value, onChangeText, password}) => {
   return (
     <S.InputArea>
       <IconSvg width="24" height="24" fill="#268596" />
-      <S.Input placeholder={placeholder} placeholderTextColor="#268596" />
+      <S.Input
+        placeholder={placeholder}
+        placeholderTextColor="#268596"
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={password}
+      />
     </S.InputArea>
   );
 };
