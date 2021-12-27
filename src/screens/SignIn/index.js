b240445f5aa38cd0418dfd4dbcca6f6e2c734873
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-26 23:03:28 -0300

    criando tipagem de components

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as S from './styles';
+import {useNavigation} from '@react-navigation/native';
 
 import BarberLogo from '../../assets/barber.svg';
 import EmailIcon from '../../assets/email.svg';
@@ -7,18 +8,42 @@ import LockIcon from '../../assets/lock.svg';
 import {SignInput} from '../../components';
 
 const SignIn = () => {
+  const navigation = useNavigation();
+
+  const [emailField, setEmailField] = useState('');
+  const [passowordField, setPasswordField] = useState('');
+
+  const handleSignClick = () => {};
+
+  const handleMessageButtomClick = () => {
+    navigation.reset({
+      routes: [{name: 'SignUp'}],
+    });
+  };
+
   return (
     <S.Container>
       <BarberLogo width="100%" height="160" />
       <S.InputArea>
-        <SignInput IconSvg={EmailIcon} placeholder="Digite seu e-mail" />
-        <SignInput IconSvg={LockIcon} placeholder="Digite sua senha" />
-        <S.CustomButton>
-          <S.CustomButtomText>Login</S.CustomButtomText>
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder="Digite seu e-mail"
+          value={emailField}
+          onChangeText={t => setEmailField(t)}
+        />
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder="Digite sua senha"
+          value={passowordField}
+          onChangeText={t => setPasswordField(t)}
+          password={true}
+        />
+        <S.CustomButton onPress={handleSignClick}>
+          <S.CustomButtomText>LOGIN</S.CustomButtomText>
         </S.CustomButton>
       </S.InputArea>
 
-      <S.SignMessage>
+      <S.SignMessage onPress={handleMessageButtomClick}>
         <S.MessageButton>Ainda não tem uma conta?</S.MessageButton>
         <S.MessageButtonBold>CADASTRE-SE AQUI!</S.MessageButtonBold>
       </S.SignMessage>
