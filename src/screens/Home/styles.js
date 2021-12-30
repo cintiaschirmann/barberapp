2e28eba24744dd176a32e7f67f1c507223eab0f9
Cintia Schirmann <cintia.schirmann@gmail.com>
2021-12-30 17:41:47 -0300

    modificando index

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -2,9 +2,52 @@ import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
-  justify-content: center;
+  background-color: #63c2d1;
+`;
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  padding: 20px;
+`;
+export const HeaderArea = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
 `;
-export const Title = styled.Text`
-  font-size: 30px;
+export const HeaderTitle = styled.Text`
+  width: 250px;
+  font-size: 24px;
+  color: #fff;
+  font-weight: bold;
+`;
+export const SearchButton = styled.TouchableOpacity`
+  width: 26px;
+  height: 26px;
+`;
+export const LoccationArea = styled.View`
+  background-color: #4eadbe;
+  height: 60px;
+  border-radius: 30px;
+  align-items: center;
+  flex-direction: row;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-top: 30px;
+`;
+export const LoccationInput = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  color: #fff;
+`;
+export const LoccationFinder = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 50px;
+`;
+
+export const ListArea = styled.View`
+  margin-top: 30px;
+  margin-bottom: 30px;
 `;
